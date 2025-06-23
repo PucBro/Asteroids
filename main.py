00b9a95a1a94a321efc46bcef0c6f6d2619bfe24
Diego <diegoo9997@gmail.com>
2025-06-22 21:28:27 -0600

    El jugador se mueve

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ from player import Player
 
 def main():
     pygame.init()
+    
+    updatable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     clock= pygame.time.Clock()
+
     dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
