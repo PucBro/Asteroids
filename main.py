3e974bd2dc0151f43d13cad6c52da5fd4281e2ff
Diego <diegoo9997@gmail.com>
2025-06-22 20:47:33 -0600

    El jugador rota

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        
        
         
     
